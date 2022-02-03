2d15b933b405ff07109723d89b2061fb71839548
Luiz Oliveira <lulinhaoliveirademelo@gmail.com>
2022-02-02 21:46:09 -0300

    Extraindo Links De Uma String e Alterando para Chamada do Arquivo Direto no Terminal

--- shortner/index.js
+++ shortner/index.js
@@ -1,18 +1,30 @@
 import chalk from 'chalk'; //biblioteca de cores
 import fs from 'fs'; //arquivos
 
+const texto ="São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList)que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)";
 
-async function getFile(path){
+function extractLink(text){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const results =[];
+    let aux;
+
+    while((aux = regex.exec(text)) !== null){
+        results.push({[aux[1]]: aux[2]})
+    }
+    return results;
+}
+
+
+export async function getFile(path){
     const encoding = 'utf-8';  
     try{
         const text = await fs.promises.readFile(path,encoding);
-        console.log(chalk.blue(text));
+        console.log(extractLink(text));
     }catch(err){
         console.log(chalk.red(err.code, "Erro Encontrado"));
     }
 }
 
 
-getFile("arquivoTeste.txt");
+//getFile("arquivoTeste.txt");
 
-console.log(chalk.red("Hellow"));
